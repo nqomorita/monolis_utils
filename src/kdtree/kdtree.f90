bc0753bd795b754c89fc1a1ad207c1eb5073ef29
Naoki MORITA <nqo.morita@gmail.com>
2023-01-23 12:49:52 +0900

    Add k-d tree

--- src/kdtree/kdtree.f90
+++ src/kdtree/kdtree.f90
@@ -4,6 +4,8 @@ module mod_monolis_utils_kdtree
   use mod_monolis_utils_error
   use mod_monolis_utils_aabb
   use mod_monolis_utils_alloc
+  use mod_monolis_utils_std_sort_R
+  use mod_monolis_utils_std_sort_I
 
   implicit none
 
@@ -11,6 +13,8 @@ module mod_monolis_utils_kdtree
 
   public :: monolis_kdtree_structure
   public :: monolis_kdtree_init_by_BB
+  public :: monolis_kdtree_get_BB_including_coordinates
+  public :: monolis_kdtree_finalize
 
   !> k-d ツリー構造体
   type monolis_kdtree_structure_main
@@ -18,8 +22,10 @@ module mod_monolis_utils_kdtree
     integer(kint) :: dim
     !> 格納する id 番号
     integer(kint) :: id
-    !> バウンディングボックス（x_min, x_max, y_min, y_max, z_min, z_max の順に格納）
+    !> root のバウンディングボックス（x_min, x_max, y_min, y_max, z_min, z_max の順に格納）
     real(kdouble) :: BB(6)
+    !> root 以下全てのバウンディングボックス（x_min, x_max, y_min, y_max, z_min, z_max の順に格納）
+    real(kdouble) :: BB_all(6)
     !> BB の中間座標
     real(kdouble) :: mid_coord(3)
     !> 中間点より座標が小さい集合の k-d ツリー構造体
@@ -42,8 +48,8 @@ contains
     implicit none
     !> k-d ツリー構造体
     type(monolis_kdtree_structure) :: monolis_kdtree
-    !> [in] バウンディングボックスの入力数
-    integer(kint), intent(in) :: n_BB
+    !> [in,out] バウンディングボックスの入力数
+    integer(kint), intent(inout) :: n_BB
     !> [in,out] バウンディングボックスの id（サイズ [n_BB]）
     integer(kint), intent(inout) :: BB_id(:)
     !> [in,out] バウンディングボックスの入力座標（サイズ [6, n_BB]、x_min, x_max, y_min, y_max, z_min, z_max の順に格納）
@@ -55,10 +61,41 @@ contains
       call monolis_std_error_stop()
     endif
 
-    call monolis_kdtree_build_by_BB(monolis_kdtree%kdtree, n_BB, BB_id, BB, 1)
+    call monolis_kdtree_init_by_BB_main(monolis_kdtree%kdtree, n_BB, BB_id, BB, 1)
   end subroutine monolis_kdtree_init_by_BB
 
-  recursive subroutine monolis_kdtree_build_by_BB(kdtree, n_BB, BB_id, BB, depth)
+  !> @ingroup kdtree
+  !> 座標を入力して座標を含むバウンディングボックスを取得
+  subroutine monolis_kdtree_get_BB_including_coordinates(monolis_kdtree, pos, n_BB, BB_id)
+    implicit none
+    !> k-d ツリー構造体
+    type(monolis_kdtree_structure) :: monolis_kdtree
+    !> [in] 入力座標
+    real(kdouble), intent(in) :: pos(3)
+    !> [out] 座標を含むバウンディングボックスの数
+    integer(kint), intent(out) :: n_BB
+    !> [out] 座標を含むバウンディングボックスの id（サイズ [n_BB]）
+    integer(kint), intent(out), allocatable :: BB_id(:)
+
+    n_BB = 0
+    call monolis_dealloc_I_1d(BB_id)
+
+    call monolis_kdtree_get_BB_including_coordinates_main(monolis_kdtree%kdtree, pos, n_BB, BB_id)
+  end subroutine monolis_kdtree_get_BB_including_coordinates
+
+  !> @ingroup kdtree
+  !> k-d ツリー構造体の終了処理
+  subroutine monolis_kdtree_finalize(monolis_kdtree)
+    implicit none
+    !> k-d ツリー構造体
+    type(monolis_kdtree_structure) :: monolis_kdtree
+
+    call monolis_kdtree_finalize_main(monolis_kdtree%kdtree)
+  end subroutine monolis_kdtree_finalize
+
+  !> @ingroup dev_kdtree
+  !> バウンディングボックス情報を入力して k-d ツリーを構築（メイン関数）
+  recursive subroutine monolis_kdtree_init_by_BB_main(kdtree, n_BB, BB_id, BB, depth)
     implicit none
     !> k-d ツリー構造体
     type(monolis_kdtree_structure_main), pointer :: kdtree
@@ -82,7 +119,7 @@ contains
     kdtree%dim = 3 - mod(depth, 3)
 
     !> 保持している全てのデータ全体の BB を取得
-    call monolis_get_aabb(BB, kdtree%BB)
+    call monolis_get_aabb_from_BB(BB, kdtree%BB_all)
 
     !> 保持しているデータごとに中間座標を取得しソート
     call monolis_alloc_R_2d(mid_coord, 3, n_BB)
@@ -93,6 +130,7 @@ contains
       mid_coord(3,i) = 0.5d0*(BB(5,i) + BB(6,i))
     enddo
 
+    call monolis_alloc_I_1d(perm, n_BB)
     call monolis_get_sequence_array_I(perm, n_BB, 1, 1)
     call monolis_qsort_R_1d_I_1d(mid_coord(kdtree%dim,:), perm, 1, n_BB)
 
@@ -105,15 +143,80 @@ contains
     call monolis_perm_array_R(BB(5,:), perm, n_BB)
     call monolis_perm_array_R(BB(6,:), perm, n_BB)
 
-    mid_id = n_BB / 2
-    kdtree%id = mid_id
+    !> 中点座標を計算し、左右で再帰的に k-d ツリーを構築
+    mid_id = max(1, n_BB / 2)
+    kdtree%id = BB_id(mid_id)
+    kdtree%BB = BB(:,mid_id)
 
     iSl = 1
     iEl = mid_id - 1
     iSr = mid_id + 1
     iEr = n_BB
 
-    call monolis_kdtree_build_by_BB(kdtree%left,  iEl - iSl + 1, BB_id(iSl:iEl), BB(:,iSl:iEl), depth + 1)
-    call monolis_kdtree_build_by_BB(kdtree%right, iEr - iSr + 1, BB_id(iSr:iEr), BB(:,iSr:iEr), depth + 1)
-  end subroutine monolis_kdtree_build_by_BB
+    call monolis_kdtree_init_by_BB_main(kdtree%left,  iEl - iSl + 1, BB_id(iSl:iEl), BB(:,iSl:iEl), depth + 1)
+    call monolis_kdtree_init_by_BB_main(kdtree%right, iEr - iSr + 1, BB_id(iSr:iEr), BB(:,iSr:iEr), depth + 1)
+  end subroutine monolis_kdtree_init_by_BB_main
+
+  !> @ingroup dev_kdtree
+  !> 座標を入力して座標を含むバウンディングボックスを取得（メイン関数）
+  recursive subroutine monolis_kdtree_get_BB_including_coordinates_main(kdtree, pos, n_BB, BB_id)
+    implicit none
+    !> k-d ツリー構造体
+    type(monolis_kdtree_structure_main), pointer :: kdtree
+    !> [in] 入力座標
+    real(kdouble), intent(in) :: pos(3)
+    !> [out] 座標を含むバウンディングボックスの数
+    integer(kint), intent(inout) :: n_BB
+    !> [out] 座標を含むバウンディングボックスの id（サイズ [n_BB]）
+    integer(kint), intent(inout), allocatable :: BB_id(:)
+    integer(kint) :: iadd(1)
+    real(kdouble) :: ths = 1.0d-8
+    logical :: is_inside
+
+    if(.not. associated(kdtree)) return
+
+    !> k-d ツリーの root のバウンディングボックスに内包されていれば、戻り値に追加
+    call monolis_check_inside_in_aabb(pos, kdtree%BB, ths, is_inside)
+
+    if(is_inside)then
+      n_BB = n_BB + 1
+      iadd(1) = kdtree%id
+      call monolis_append_I_1d(BB_id, 1, iadd)
+    endif
+
+    !> 入力座標が child の全てのバウンディングボックスに内包されていれば検索を継続
+    if(associated(kdtree%left))then
+      if(kdtree%left%BB_all(2*kdtree%dim - 1) - ths <= pos(kdtree%dim) .and. &
+         pos(kdtree%dim) <= kdtree%left%BB_all(2*kdtree%dim) + ths)then
+        call monolis_kdtree_get_BB_including_coordinates_main(kdtree%left , pos, n_BB, BB_id)
+      endif
+    endif
+
+    if(associated(kdtree%right))then
+      if(kdtree%right%BB_all(2*kdtree%dim - 1) - ths <= pos(kdtree%dim) .and. &
+         pos(kdtree%dim) <= kdtree%right%BB_all(2*kdtree%dim) + ths)then
+        call monolis_kdtree_get_BB_including_coordinates_main(kdtree%right, pos, n_BB, BB_id)
+      endif
+    endif
+  end subroutine monolis_kdtree_get_BB_including_coordinates_main
+
+  !> @ingroup dev_kdtree
+  !> k-d ツリー構造体の終了処理（メイン関数）
+  recursive subroutine monolis_kdtree_finalize_main(kdtree)
+    implicit none
+    !> k-d ツリー構造体
+    type(monolis_kdtree_structure_main), pointer :: kdtree
+
+    if(associated(kdtree%left))then
+      call monolis_kdtree_finalize_main(kdtree%left)
+      deallocate(kdtree%left)
+      kdtree%left => null()
+    endif
+
+    if(associated(kdtree%right))then
+      call monolis_kdtree_finalize_main(kdtree%right)
+      deallocate(kdtree%right)
+      kdtree%right => null()
+    endif
+  end subroutine monolis_kdtree_finalize_main
 end module mod_monolis_utils_kdtree
--- test/kdtree/kdtree_test.f90
+++ test/kdtree/kdtree_test.f90
@@ -2,8 +2,9 @@
 module mod_monolis_utils_kdtree_test
   use mod_monolis_utils_define_prm
   use mod_monolis_utils_error
-  use mod_monolis_utils_aabb
   use mod_monolis_utils_std_test
+  use mod_monolis_utils_kdtree
+  use mod_monolis_utils_std_sort_I
   implicit none
 
 contains
@@ -13,16 +14,201 @@ contains
     implicit none
 
     call monolis_kdtree_init_test()
+    call monolis_kdtree_overlap_test()
   end subroutine monolis_utils_kdtree_test
 
   !> unit test
   subroutine monolis_kdtree_init_test()
     implicit none
+    type(monolis_kdtree_structure) :: monolis_kdtree
+    integer(kint) :: n_BB
+    integer(kint) :: BB_id(5)
+    real(kdouble) :: BB(6,5), pos(3)
+    integer(kint), allocatable :: ids(:)
 
     call monolis_std_log_string("monolis_kdtree_init_test")
 
-    !call monolis_aabb_init(monolis_aabb, BB, div)
+    !> initialize
+    BB_id(1) = 10
+    BB_id(2) = 20
+    BB_id(3) = 30
+    BB_id(4) = 40
+    BB_id(5) = 50
 
-    !call monolis_aabb_finalize(monolis_aabb)
+    BB(1,1) =  1.0d0; BB(2,1) =  2.0d0; BB(3,1) = 51.0d0; BB(4,1) = 52.0d0; BB(5,1) = 101.0d0; BB(6,1) = 102.0d0;
+    BB(1,2) = 11.0d0; BB(2,2) = 12.0d0; BB(3,2) = 61.0d0; BB(4,2) = 62.0d0; BB(5,2) = 111.0d0; BB(6,2) = 112.0d0;
+    BB(1,3) = 21.0d0; BB(2,3) = 22.0d0; BB(3,3) = 71.0d0; BB(4,3) = 72.0d0; BB(5,3) = 121.0d0; BB(6,3) = 122.0d0;
+    BB(1,4) = 31.0d0; BB(2,4) = 32.0d0; BB(3,4) = 81.0d0; BB(4,4) = 82.0d0; BB(5,4) = 131.0d0; BB(6,4) = 132.0d0;
+    BB(1,5) = 41.0d0; BB(2,5) = 42.0d0; BB(3,5) = 91.0d0; BB(4,5) = 92.0d0; BB(5,5) = 141.0d0; BB(6,5) = 142.0d0;
+
+    n_BB = 5
+
+    call monolis_kdtree_init_by_BB(monolis_kdtree, n_BB, BB_id, BB)
+
+    !> case 1
+    pos(1) = 1.0d0
+    pos(2) = 1.0d0
+    pos(3) = 1.0d0
+
+    call monolis_kdtree_get_BB_including_coordinates(monolis_kdtree, pos, n_BB, ids)
+
+    call monolis_test_check_eq_I1("monolis_kdtree_init_test 1", n_BB, 0)
+
+    !> case 2
+    pos(1) = 1.0d0
+    pos(2) = 51.0d0
+    pos(3) = 101.0d0
+
+    call monolis_kdtree_get_BB_including_coordinates(monolis_kdtree, pos, n_BB, ids)
+
+    call monolis_test_check_eq_I1("monolis_kdtree_init_test 2", n_BB, 1)
+    call monolis_test_check_eq_I1("monolis_kdtree_init_test 2", ids(1), 10)
+
+    !> case 3
+    pos(1) = 11.0d0
+    pos(2) = 61.0d0
+    pos(3) = 111.0d0
+
+    call monolis_kdtree_get_BB_including_coordinates(monolis_kdtree, pos, n_BB, ids)
+
+    call monolis_test_check_eq_I1("monolis_kdtree_init_test 3", n_BB, 1)
+    call monolis_test_check_eq_I1("monolis_kdtree_init_test 3", ids(1), 20)
+
+    !> case 4
+    pos(1) = 21.0d0
+    pos(2) = 71.0d0
+    pos(3) = 121.0d0
+
+    call monolis_kdtree_get_BB_including_coordinates(monolis_kdtree, pos, n_BB, ids)
+
+    call monolis_test_check_eq_I1("monolis_kdtree_init_test 4", n_BB, 1)
+    call monolis_test_check_eq_I1("monolis_kdtree_init_test 4", ids(1), 30)
+
+    !> case 5
+    pos(1) = 31.0d0
+    pos(2) = 81.0d0
+    pos(3) = 131.0d0
+
+    call monolis_kdtree_get_BB_including_coordinates(monolis_kdtree, pos, n_BB, ids)
+
+    call monolis_test_check_eq_I1("monolis_kdtree_init_test 5", n_BB, 1)
+    call monolis_test_check_eq_I1("monolis_kdtree_init_test 5", ids(1), 40)
+
+    !> case 6
+    pos(1) = 41.0d0
+    pos(2) = 91.0d0
+    pos(3) = 141.0d0
+
+    call monolis_kdtree_get_BB_including_coordinates(monolis_kdtree, pos, n_BB, ids)
+
+    call monolis_test_check_eq_I1("monolis_kdtree_init_test 6", n_BB, 1)
+    call monolis_test_check_eq_I1("monolis_kdtree_init_test 6", ids(1), 50)
+
+    call monolis_kdtree_finalize(monolis_kdtree)
   end subroutine monolis_kdtree_init_test
+
+  subroutine monolis_kdtree_overlap_test()
+    implicit none
+    type(monolis_kdtree_structure) :: monolis_kdtree
+    integer(kint) :: n_BB
+    integer(kint) :: BB_id(5), ids_ans(5)
+    real(kdouble) :: BB(6,5), pos(3)
+    integer(kint), allocatable :: ids(:)
+
+    call monolis_std_log_string("monolis_kdtree_overlap_test")
+
+    !> initialize
+    BB_id(1) = 50
+    BB_id(2) = 40
+    BB_id(3) = 30
+    BB_id(4) = 20
+    BB_id(5) = 10
+
+    BB(1,1) = 0.0d0; BB(2,1) = 5.0d0; BB(3,1) = 0.0d0; BB(4,1) = 5.0d0; BB(5,1) = 0.0d0; BB(6,1) = 5.0d0;
+    BB(1,2) = 0.0d0; BB(2,2) = 4.0d0; BB(3,2) = 0.0d0; BB(4,2) = 4.0d0; BB(5,2) = 0.0d0; BB(6,2) = 4.0d0;
+    BB(1,3) = 0.0d0; BB(2,3) = 3.0d0; BB(3,3) = 0.0d0; BB(4,3) = 3.0d0; BB(5,3) = 0.0d0; BB(6,3) = 3.0d0;
+    BB(1,4) = 0.0d0; BB(2,4) = 2.0d0; BB(3,4) = 0.0d0; BB(4,4) = 2.0d0; BB(5,4) = 0.0d0; BB(6,4) = 2.0d0;
+    BB(1,5) = 0.0d0; BB(2,5) = 1.0d0; BB(3,5) = 0.0d0; BB(4,5) = 1.0d0; BB(5,5) = 0.0d0; BB(6,5) = 1.0d0;
+
+    n_BB = 5
+
+    call monolis_kdtree_init_by_BB(monolis_kdtree, n_BB, BB_id, BB)
+
+    !> case 1
+    pos(1) = 1.0d0
+    pos(2) = 1.0d0
+    pos(3) = 1.0d0
+
+    call monolis_kdtree_get_BB_including_coordinates(monolis_kdtree, pos, n_BB, ids)
+    call monolis_qsort_I_1d(ids, 1, 5)
+
+    ids_ans(1) = 10
+    ids_ans(2) = 20
+    ids_ans(3) = 30
+    ids_ans(4) = 40
+    ids_ans(5) = 50
+
+    call monolis_test_check_eq_I1("monolis_kdtree_overlap_test 1", n_BB, 5)
+    call monolis_test_check_eq_I("monolis_kdtree_overlap_test 1", ids, ids_ans)
+
+    !> case 2
+    pos(1) = 2.0d0
+    pos(2) = 2.0d0
+    pos(3) = 2.0d0
+
+    call monolis_kdtree_get_BB_including_coordinates(monolis_kdtree, pos, n_BB, ids)
+    call monolis_qsort_I_1d(ids, 1, 4)
+
+    ids_ans(1) = 20
+    ids_ans(2) = 30
+    ids_ans(3) = 40
+    ids_ans(4) = 50
+
+    call monolis_test_check_eq_I1("monolis_kdtree_overlap_test 2", n_BB, 4)
+    call monolis_test_check_eq_I("monolis_kdtree_overlap_test 2", ids, ids_ans(1:4))
+
+    !> case 3
+    pos(1) = 3.0d0
+    pos(2) = 3.0d0
+    pos(3) = 3.0d0
+
+    call monolis_kdtree_get_BB_including_coordinates(monolis_kdtree, pos, n_BB, ids)
+    call monolis_qsort_I_1d(ids, 1, 3)
+
+    ids_ans(1) = 30
+    ids_ans(2) = 40
+    ids_ans(3) = 50
+
+    call monolis_test_check_eq_I1("monolis_kdtree_overlap_test 3", n_BB, 3)
+    call monolis_test_check_eq_I("monolis_kdtree_overlap_test 3", ids, ids_ans(1:3))
+
+    !> case 4
+    pos(1) = 4.0d0
+    pos(2) = 4.0d0
+    pos(3) = 4.0d0
+
+    call monolis_kdtree_get_BB_including_coordinates(monolis_kdtree, pos, n_BB, ids)
+    call monolis_qsort_I_1d(ids, 1, 2)
+
+    ids_ans(1) = 40
+    ids_ans(2) = 50
+
+    call monolis_test_check_eq_I1("monolis_kdtree_overlap_test 4", n_BB, 2)
+    call monolis_test_check_eq_I("monolis_kdtree_overlap_test 4", ids, ids_ans(1:2))
+
+    !> case 5
+    pos(1) = 5.0d0
+    pos(2) = 5.0d0
+    pos(3) = 5.0d0
+
+    call monolis_kdtree_get_BB_including_coordinates(monolis_kdtree, pos, n_BB, ids)
+    call monolis_qsort_I_1d(ids, 1, 1)
+
+    ids_ans(1) = 50
+
+    call monolis_test_check_eq_I1("monolis_kdtree_overlap_test 5", n_BB, 1)
+    call monolis_test_check_eq_I("monolis_kdtree_overlap_test 5", ids, ids_ans(1:1))
+
+    call monolis_kdtree_finalize(monolis_kdtree)
+  end subroutine monolis_kdtree_overlap_test
 end module mod_monolis_utils_kdtree_test
