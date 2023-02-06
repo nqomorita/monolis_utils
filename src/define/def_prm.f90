3299d2b497eb98cafd0fb3f6ba5484c6f7467267
Naoki MORITA <nqo.morita@gmail.com>
2023-02-06 12:50:36 +0900

    Add monolis_I_false flag

--- src/define/def_prm.f90
+++ src/define/def_prm.f90
@@ -13,6 +13,10 @@ module mod_monolis_utils_define_prm
   integer(kint), parameter :: monolis_fail = 1
   !> monolis ライブラリにおける文字列長さ
   integer(kint), parameter :: monolis_charlen = 256
+  !> monolis ライブラリにおける整数型の true フラグ
+  integer(kint), parameter :: monolis_I_true = 1
+  !> monolis ライブラリにおける整数型の false フラグ
+  integer(kint), parameter :: monolis_I_false = 0
 
   !> monolis ライブラリにおけるテスト機能の一致判定閾値
   real(kdouble), parameter :: monolis_test_ths = 1.0d-8
