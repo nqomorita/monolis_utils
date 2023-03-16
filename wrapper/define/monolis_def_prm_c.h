524a80c94895aae058e3b67a66c2c1fbc1e33edc
Naoki MORITA <nqo.morita@gmail.com>
2023-03-16 14:51:00 +0900

    Fix monolis_def_prm_c.h

--- wrapper/define/monolis_def_prm_c.h
+++ wrapper/define/monolis_def_prm_c.h
@@ -6,15 +6,15 @@
 extern "C" {
 #endif
 
-const int monolis_success = 0
+const int monolis_success = 0;
 
-const int monolis_fail = 1
+const int monolis_fail = 1;
 
-const int monolis_charlen = 256
+const int monolis_charlen = 256;
 
-const int monolis_I_true = 1
+const int monolis_I_true = 1;
 
-const int monolis_I_false = 0
+const int monolis_I_false = 0;
 
 #ifdef __cplusplus
 }
