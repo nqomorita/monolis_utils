a7887373c3d4047450a0b4042c764a08d48dd24a
Naoki MORITA <nqo.morita@gmail.com>
2023-03-23 13:34:16 +0900

    Add monolis_comm_table_c_test

--- wrapper_test/com/monolis_comm_table_c_test.c
+++ wrapper_test/com/monolis_comm_table_c_test.c
@@ -35,4 +35,38 @@ void monolis_comm_table_c_test()
   }
 
   monolis_com_get_comm_table_parallel(n_internal_vertex, n_vertex, vertex_id, &com);
+
+  if(monolis_mpi_get_global_my_rank() == 0){
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 1", com.my_rank, 0);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 2", com.comm_size, 2);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 3", com.n_internal_vertex, 3);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 4", com.recv_n_neib, 1);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 5", com.recv_neib_pe[0], 1);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 6", com.recv_index[0], 0);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 7", com.recv_index[1], 2);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 8", com.recv_item[0], 4);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 9", com.recv_item[1], 5);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 10", com.send_n_neib, 1);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 11", com.send_neib_pe[0], 1);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 12", com.send_index[0], 0);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 13", com.send_index[1], 2);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 14", com.send_item[0], 2);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 15", com.send_item[1], 3);
+  } else {
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 1", com.my_rank, 1);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 2", com.comm_size, 2);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 3", com.n_internal_vertex, 3);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 4", com.recv_n_neib, 1);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 5", com.recv_neib_pe[0], 0);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 6", com.recv_index[0], 0);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 7", com.recv_index[1], 2);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 8", com.recv_item[0], 4);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 9", com.recv_item[1], 5);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 10", com.send_n_neib, 1);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 11", com.send_neib_pe[0], 0);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 12", com.send_index[0], 0);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 13", com.send_index[1], 2);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 14", com.send_item[0], 1);
+    monolis_test_check_eq_I1("monolis_com_get_comm_table_parallel 15", com.send_item[1], 2);
+  }
 }
