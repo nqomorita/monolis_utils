1c3cd1ff5b3f6494a5cb5533c7db346f93171fd0
Naoki MORITA <nqo.morita@gmail.com>
2023-03-25 09:28:37 +0900

    Fix run.sh

--- src_test/run.sh
+++ src_test/run.sh
@@ -19,4 +19,4 @@ OUT=driver/output
 
 ./monolis_utils_test -test_i 1 -test_r 2.0 -test_s test_string -i a.txt -o b.txt -in c.txt -ie d.txt -n 2 2 10.0 20.0
 
-#mpirun -np 2 ./monolis_utils_test -test_i 1 -test_r 2.0 -test_s test_string -i a.txt -o b.txt -in c.txt -ie d.txt -n 2 2 10.0 20.0
+mpirun -np 2 ./monolis_utils_test -test_i 1 -test_r 2.0 -test_s test_string -i a.txt -o b.txt -in c.txt -ie d.txt -n 2 2 10.0 20.0
