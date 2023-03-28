aa7df2f05af6e3a7eff77cd186c5e5fb7652be71
Naoki MORITA <nqo.morita@gmail.com>
2023-03-28 18:18:13 +0900

    Fix run.sh test

--- src_test/run.sh
+++ src_test/run.sh
@@ -2,6 +2,8 @@
 
 INPF=driver/input.f
 OUTF=driver/output.f
+INPC=driver/input.c
+OUTC=driver/output.c
 
 ../bin/monolis_dbc_all_surf_hex -i ${INPF}/elem.hex.dat -o ${OUTF}/dbc.hex.dat 2 1.0 2.0
 
@@ -16,6 +18,20 @@ OUTF=driver/output.f
 ../bin/monolis_h_refiner_tet -in ${INPF}/node.tet.2elem.dat -ie ${INPF}/elem.tet.2elem.dat -on ${OUTF}/node.ref.h.tet.dat -oe ${OUTF}/elem.ref.h.tet.dat
 
 ../bin/monolis_h_refiner_hex -in ${INPF}/node.hex.2elem.dat -ie ${INPF}/elem.hex.2elem.dat -on ${OUTF}/node.ref.h.hex.dat -oe ${OUTF}/elem.ref.h.hex.dat
+
+../bin/monolis_dbc_all_surf_hex -i ${INPC}/elem.hex.dat -o ${OUTC}/dbc.hex.dat 2 1.0 2.0
+
+../bin/monolis_dbc_all_surf_tet -i ${INPC}/elem.tet.dat -o ${OUTC}/dbc.tet.dat 2 1.0 2.0
+
+../bin/monolis_extract_all_surf_hex -i ${INPC}/elem.hex.dat -o ${OUTC}/surf.hex.dat
+
+../bin/monolis_extract_all_surf_tet -i ${INPC}/elem.tet.dat -o ${OUTC}/surf.tet.dat
+
+../bin/monolis_p_refiner_tet -in ${INPC}/node.tet.dat -ie ${INPC}/elem.tet.dat -on ${OUTC}/node.ref.p.tet.dat -oe ${OUTC}/elem.ref.p.tet.dat
+
+../bin/monolis_h_refiner_tet -in ${INPC}/node.tet.2elem.dat -ie ${INPC}/elem.tet.2elem.dat -on ${OUTC}/node.ref.h.tet.dat -oe ${OUTC}/elem.ref.h.tet.dat
+
+../bin/monolis_h_refiner_hex -in ${INPC}/node.hex.2elem.dat -ie ${INPC}/elem.hex.2elem.dat -on ${OUTC}/node.ref.h.hex.dat -oe ${OUTC}/elem.ref.h.hex.dat
 
 ./monolis_utils_test -test_i 1 -test_r 2.0 -test_s test_string -i a.txt -o b.txt -in c.txt -ie d.txt -n 2 2 10.0 20.0
 
