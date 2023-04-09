0a8667de13251cc6353918aedcef57d6ecc660ab
Naoki MORITA <nqo.morita@gmail.com>
2023-04-09 09:28:15 +0900

    Fix test header #2

--- src_test/driver/driver_util_test.f90
+++ src_test/driver/driver_util_test.f90
@@ -20,32 +20,32 @@ contains
     complex(kdouble), allocatable :: cval(:)
     logical :: is_get
 
-    call monolis_std_log_string("monolis_driver_get_arg_dbc_all_test R")
+    call monolis_std_log_string("monolis_driver_get_arg_dbc_all_R R")
 
     call monolis_driver_get_arg_dbc_all_R(n_dof, rval, is_get)
 
-    call monolis_test_check_eq_L1("monolis_driver_get_arg_dbc_all_test R 1", is_get, .true.)
+    call monolis_test_check_eq_L1("monolis_driver_get_arg_dbc_all_R 1", is_get, .true.)
 
-    call monolis_test_check_eq_I1("monolis_driver_get_arg_dbc_all_test R 2", n_dof, 2)
+    call monolis_test_check_eq_I1("monolis_driver_get_arg_dbc_all_R 2", n_dof, 2)
 
     r_ans(1) = 10.0d0
     r_ans(2) = 20.0d0
 
-    call monolis_test_check_eq_R ("monolis_driver_get_arg_dbc_all_test R 3", rval, r_ans)
+    call monolis_test_check_eq_R ("monolis_driver_get_arg_dbc_all_R 3", rval, r_ans)
 
 
 
-    call monolis_std_log_string("monolis_driver_get_arg_dbc_all_test C")
+    call monolis_std_log_string("monolis_driver_get_arg_dbc_all_C")
 
     call monolis_driver_get_arg_dbc_all_C(n_dof, cval, is_get)
 
-    call monolis_test_check_eq_L1("monolis_driver_get_arg_dbc_all_test C 1", is_get, .true.)
+    call monolis_test_check_eq_L1("monolis_driver_get_arg_dbc_all_C 1", is_get, .true.)
 
-    call monolis_test_check_eq_I1("monolis_driver_get_arg_dbc_all_test C 2", n_dof, 2)
+    call monolis_test_check_eq_I1("monolis_driver_get_arg_dbc_all_C 2", n_dof, 2)
 
     c_ans(1) = (10.0d0, 20.0d0)
     c_ans(2) = (30.0d0, 40.0d0)
 
-    call monolis_test_check_eq_C ("monolis_driver_get_arg_dbc_all_test C 3", cval, c_ans)
+    call monolis_test_check_eq_C ("monolis_driver_get_arg_dbc_all_C 3", cval, c_ans)
   end subroutine monolis_driver_get_arg_dbc_all_test
 end module mod_monolis_driver_util_test
