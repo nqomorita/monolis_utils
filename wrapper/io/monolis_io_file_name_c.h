58349596e43c1a2ae691209a80a26620a7c97bac
takumi308 <omuraisusan@gmail.com>
2023-06-13 18:05:08 +0900

    Fix monolis_io_file_name_c.h

--- wrapper/io/monolis_io_file_name_c.h
+++ wrapper/io/monolis_io_file_name_c.h
@@ -7,18 +7,21 @@ extern "C" {
 #endif
 
 /**
- * @brief データ通信する recv 隣接領域の取得（並列実行版）
- * @param[in] n_internal_vertex 分割領域における内部計算点数
- * @ingroup com
+ * @brief 並列計算用読み込みファイル名の取得（グローバルコミュニケータから指定）
+ * @param[in] dir_name 入力ディレクトリ名
+ * @param[in] file_name 入力ファイル名
+ * @ingroup io
  */
 const char* monolis_get_global_input_file_name(
   const char* dir_name,
   const char* file_name);
 
 /**
- * @brief データ通信する recv 隣接領域の取得（並列実行版）
- * @param[in] n_internal_vertex 分割領域における内部計算点数
- * @ingroup com
+ * @brief 並列計算用読み込みファイル名の取得（ローカルコミュニケータから指定）
+ * @param[in] dir_name 入力ディレクトリ名
+ * @param[in] file_name 入力ファイル名
+ * @param[in] comm MPI コミュニケータ
+ * @ingroup io
  */
 const char* monolis_get_local_input_file_name(
   const char* dir_name,
@@ -26,18 +29,21 @@ const char* monolis_get_local_input_file_name(
   int         comm);
 
 /**
- * @brief データ通信する recv 隣接領域の取得（並列実行版）
- * @param[in] n_internal_vertex 分割領域における内部計算点数
- * @ingroup com
+ * @brief 並列計算用書き出しファイル名の取得（グローバルコミュニケータから指定）
+ * @param[in] dir_name 入力ディレクトリ名
+ * @param[in] file_name 入力ファイル名
+ * @ingroup io
  */
 const char* monolis_get_global_output_file_name(
   const char* dir_name,
   const char* file_name);
 
 /**
- * @brief データ通信する recv 隣接領域の取得（並列実行版）
- * @param[in] n_internal_vertex 分割領域における内部計算点数
- * @ingroup com
+ * @brief 並列計算用書き出しファイル名の取得（ローカルコミュニケータから指定）
+ * @param[in] dir_name 入力ディレクトリ名
+ * @param[in] file_name 入力ファイル名
+ * @param[in] comm MPI コミュニケータ
+ * @ingroup io
  */
 const char* monolis_get_local_output_file_name(
   const char* dir_name,
@@ -45,9 +51,11 @@ const char* monolis_get_local_output_file_name(
   int         comm);
 
 /**
- * @brief データ通信する recv 隣接領域の取得（並列実行版）
- * @param[in] n_internal_vertex 分割領域における内部計算点数
- * @ingroup com
+ * @brief 並列計算用書き出しファイル名の取得（領域番号から指定）
+ * @param[in] dir_name 入力ディレクトリ名
+ * @param[in] file_name 入力ファイル名
+ * @param[in] domain_id 領域番号
+ * @ingroup io
  */
 const char* monolis_get_output_file_name_by_domain_id(
   const char* dir_name,
